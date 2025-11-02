a4edd714fdbe163cfda12e527df0153624af4e23
stulebaev <salavat-tulebaev@mail.ru>
2025-11-02 11:54:42 +0100

    Modified getopt.cmake

--- cmake/getopt.cmake
+++ cmake/getopt.cmake
@@ -1,9 +1,10 @@
 # SPDX-License-Identifier: MIT
-# Copyright (c) 2018-2023, Advanced Micro Devices, Inc. All rights reserved.
-
-add_library(getopt::getopt STATIC IMPORTED)
+# Copyright (c) 2018-2025, Advanced Micro Devices, Inc. All rights reserved.
 
 if(WIN32)
+  add_library(getopt::getopt STATIC IMPORTED)
   set_property(TARGET getopt::getopt PROPERTY IMPORTED_LOCATION "${PROJECT_SOURCE_DIR}/wingetopt/wingetopt.lib")
   target_include_directories(getopt::getopt INTERFACE wingetopt/src)
+else()
+  add_library(getopt::getopt INTERFACE IMPORTED GLOBAL)
 endif()
